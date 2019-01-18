396c0ef1d17a86e454cd0dc88c788d6836ca19ec
OzHache <OzHache@gmail.com>
2019-01-18 13:36:23 -0800

    Session 4
    After the Ball impacts

--- Pong Build/Assets/Scripts/Ball.cs
+++ Pong Build/Assets/Scripts/Ball.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public Rigidbody2D rb;          // This is a reference to the rigidbody and will eliminate long code
+    private float velX;
+    private float velY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        velX = rb.velocity.x;
+        velY = rb.velocity.y;
     }
 
     public void Launch()
@@ -30,23 +35,24 @@ public class Ball : MonoBehaviour
         transform.Rotate(Vector3.forward * UnityEngine.Random.Range(0f, 360f));
         //set velocity
 
-        gameObject.GetComponent<Rigidbody2D>().velocity = transform.right * 8f;
+        rb.velocity = transform.right * 8f;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Capture the Velocity of the Ball
-        float velX = gameObject.GetComponent<Rigidbody2D>().velocity.x;
-        float velY = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+
 
         //if we hit a wall flip the Y 
-        /*
-         * Before we add this collision we need to add tags to the Walls 
-         */ 
-       
+        if (collision.collider.CompareTag("Wall"))
+        {
+            rb.velocity = new Vector2(velX, -velY);
+        }
+
         //if we hit a paddle flip the X
-        /*
-         *Before we add this collision we need to add tags to the Paddles 
-         */
+        if (collision.collider.CompareTag("Paddle"))
+        {
+            rb.velocity = new Vector2(-velX, velY);
+        }
     }
 
     //Method to check to see if the Ball has triggered the goal. 
